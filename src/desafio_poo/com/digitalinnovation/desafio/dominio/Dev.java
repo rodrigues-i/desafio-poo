0db5b4a66351e8c5b0f6f7d970705cdc5bd8bab4
Lucas Rodrigues <rodrigues-i@outlook.com>
2023-02-16 13:31:22 -0300

    Create the entities Bootcamp and Dev

---
+++ src/desafio_poo/com/digitalinnovation/desafio/dominio/Dev.java
@@ -0,0 +1,61 @@
+package desafio_poo.com.digitalinnovation.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+	private String name;
+	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+	
+	public void inscreverBootcamp(Bootcamp bootcamp) {
+	}
+	
+	public void progredir() {
+	}
+	
+	public void calcularTotalXp() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Conteudo> getConteudosInscritos() {
+		return conteudosInscritos;
+	}
+
+	public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+		this.conteudosInscritos = conteudosInscritos;
+	}
+
+	public Set<Conteudo> getConteudosConcluidos() {
+		return conteudosConcluidos;
+	}
+
+	public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+		this.conteudosConcluidos = conteudosConcluidos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dev other = (Dev) obj;
+		return Objects.equals(name, other.name);
+	}
+}
--- src/desafio_poo/com/digitalinnovation/desafio/dominio/Main.java
+++ src/desafio_poo/com/digitalinnovation/desafio/dominio/Main.java
@@ -7,17 +7,17 @@ public class Main {
 		
 		Curso cursoJava = new Curso();
 		cursoJava.setTitulo("Curso de java");
-		cursoJava.setDescricao("Aprendendo mais sobre orientação a objetos");
+		cursoJava.setDescricaao("Aprendendo mais sobre orientação a objetos");
 		cursoJava.setCargaHoraria(8);
 		
 		Curso cursoJs = new Curso();
 		cursoJs.setTitulo("Curso de javascript");
-		cursoJs.setDescricao("Introdução a promises");
+		cursoJs.setDescricaao("Introdução a promises");
 		cursoJs.setCargaHoraria(3);
 		
 		Mentoria mentoriaJava = new Mentoria();
 		mentoriaJava.setTitulo("Introdução a java");
-		mentoriaJava.setDescricao("Começando a aprender como java");
+		mentoriaJava.setDescricaao("Começando a aprender como java");
 		mentoriaJava.setData(LocalDate.now());
 		
 		System.out.println(cursoJava);
